4ecd352cf86a8a634ac576684b3130f6e5ecac17
ozlphrt <ozalph@gmail.com>
2026-02-01 12:21:18 +0300

    Force Deploy v1.1.1: Cache Bust

--- public/sw.js
+++ public/sw.js
@@ -1,14 +1,26 @@
-const CACHE_NAME = 'flagtest-v1';
+const CACHE_NAME = 'flagtest-v1.1.0';
 const ASSETS = [
-  '/FlagTest/',
-  '/FlagTest/index.html',
-  '/FlagTest/manifest.json'
+  './',
+  './index.html',
+  './manifest.json'
 ];
 
 self.addEventListener('install', (event) => {
+  self.skipWaiting(); // Force update
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.map((key) => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      })
+    ))
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
